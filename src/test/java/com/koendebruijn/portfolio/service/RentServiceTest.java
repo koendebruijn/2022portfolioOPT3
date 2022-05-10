979e8277012200886d4e662f542df37cff853ed0
koendebruijn <bruijnk@gmail.com>
2022-05-10 11:32:41 +0200

    Added extra tests

--- src/test/java/com/koendebruijn/portfolio/service/RentServiceTest.java
+++ src/test/java/com/koendebruijn/portfolio/service/RentServiceTest.java
@@ -18,9 +18,35 @@ public class RentServiceTest {
     }
 
     @Test
+    public void calculateExtraCost_ShouldGiveExtraCost_Age18() {
+        // Given
+        int age = 18;
+        double expected = 0.05;
+
+        // When
+        double extraCosts = underTest.calculateExtraCost(age);
+
+        // Then
+        Assert.assertEquals(expected, extraCosts, 0.1);
+    }
+
+    @Test
     public void calculateExtraCost_ShouldGiveExtraCost_AgeUnder23() {
         // Given
         int age = 21;
+        double expected = 0.05;
+
+        // When
+        double extraCosts = underTest.calculateExtraCost(age);
+
+        // Then
+        Assert.assertEquals(expected, extraCosts, 0.1);
+    }
+
+    @Test
+    public void calculateExtraCost_ShouldGiveExtraCost_Age23() {
+        // Given
+        int age = 23;
         double expected = 0.05;
 
         // When
